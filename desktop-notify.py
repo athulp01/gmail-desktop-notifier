b0510b928708bc1098991af32a576c24034c3c2d
Athul P <athul929@gmail.com>
2018-03-31 11:38:46 +0000

    add gmail icon

--- desktop-notify.py
+++ desktop-notify.py
@@ -3,6 +3,7 @@ import os
 import base64
 import email
 import notify2
+import math
 
 from apiclient import errors
 from apiclient import discovery
@@ -13,7 +14,9 @@ from oauth2client.file import Storage
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'gmail-desktop-notify'
-TIMESTAMP = open('prev_time', 'w+')
+TIMESTAMP = open('prev_time', 'r+')
+ICON = '/home/athulp/Downloads/gmail.svg'
+EMAIL_ID = 'athul929@gmail.com'
 notify2.init('Emails')
 
 def get_credentials():
@@ -46,11 +49,14 @@ def ListMessagesMatchingQuery(service, user_id, max = 10, query=''):
 
 def GetMessage(service, user_id, msg_id):
 	message = service.users().messages().get(userId=user_id, id=msg_id).execute()
-	notify2.Notification('New message from '+message['payload']['headers'][1]['value'], message['snippet']).show()
+	print(TIMESTAMP.read())
+	print(type(message['internalDate']))
+	notify2.Notification('New message from '+message['payload']['headers'][1]['value'], message['snippet'], ICON ).show()
 	if TIMESTAMP.closed is False:
+		print('hi')
 		TIMESTAMP.write(message['internalDate'])
 		TIMESTAMP.close()
 	return message
 
-for message in ListMessagesMatchingQuery(service, 'athul929@gmail.com', 1, 'is:unread'):
-	GetMessage(service, 'athul929@gmail.com', message['id'])
+for message in ListMessagesMatchingQuery(service, EMAIL_ID, 1, 'is:unread category:primary'):
+	GetMessage(service, EMAIL_ID, message['id'])
